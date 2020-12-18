571c97eb8984441a7e6adb1f7b692417ca24070f
chengdongzhang0710 <chengdongzhang0710@outlook.com>
2020-12-18 05:26:30 -0500

    Update stochastic lot sizing algorithm

--- stochastic_lot_sizing.py
+++ stochastic_lot_sizing.py
@@ -1,6 +1,9 @@
 # Reference: 512-F20-Topic7 Stochastic Lot-Sizing without Backlog
 import math
 import numpy as np
+import pandas as pd
+
+from typing import List, Tuple, Dict
 
 
 # Problem Data Input
@@ -19,13 +22,13 @@ class Param:
 
 # Regard 1000 as 1
 class ParamInstance(Param):
-    setup_cost = 1000000
-    production_cost = 200000
-    inventory_cost = 1000
+    setup_cost = 2000000
+    production_cost = 172800
+    inventory_cost = 2100
     unit_revenue = 240000
-    salvage_price = 100000
+    salvage_price = 99000
     inventory_limit = 100
-    production_limit = 100
+    production_limit = 80
     time_horizon = 6
     penalty = 100000
     shift_period = 2
@@ -62,7 +65,7 @@ def prob_distribution(x: float) -> float:
     return y
 
 
-def calculate_demand_distribution(demand: Demand, division: int) -> dict:
+def calculate_demand_distribution(demand: Demand, division: int) -> Dict[int, float]:
     infect_rate = np.linspace(0, 0.1, division)
     prob = []
     total_demand = []
@@ -85,7 +88,7 @@ def calculate_demand_distribution(demand: Demand, division: int) -> dict:
 
 
 # Markov Decision Process
-def calculate_lost_demand(inv_limit: int, demand_dist: dict) -> dict:
+def calculate_lost_demand(inv_limit: int, demand_dist: Dict[int, float]) -> Dict[int, float]:
     lost_demand = {}
     for level in range(inv_limit + 1):
         expected_lost = 0
@@ -95,7 +98,7 @@ def calculate_lost_demand(inv_limit: int, demand_dist: dict) -> dict:
     return lost_demand
 
 
-def calculate_revenue(inv_limit: int, unit_revenue: float, demand_dist: dict) -> dict:
+def calculate_revenue(inv_limit: int, unit_revenue: float, demand_dist: Dict[int, float]) -> Dict[int, float]:
     revenue = {}
     for level in range(inv_limit + 1):
         expected_revenue = 0
@@ -105,7 +108,7 @@ def calculate_revenue(inv_limit: int, unit_revenue: float, demand_dist: dict) ->
     return revenue
 
 
-def calculate_reward(param: Param, demand_dist: dict) -> dict:
+def calculate_reward(param: Param, demand_dist: Dict[int, float]) -> Dict[Tuple[int, int], float]:
     lost_demand = calculate_lost_demand(param.inventory_limit, demand_dist)
     revenue = calculate_revenue(param.inventory_limit, param.unit_revenue, demand_dist)
     reward = {}
@@ -120,7 +123,7 @@ def calculate_reward(param: Param, demand_dist: dict) -> dict:
     return reward
 
 
-def calculate_transition_matrix(inv_limit: int, demand_dist: dict) -> list:
+def calculate_transition_matrix(inv_limit: int, demand_dist: Dict[int, float]) -> List[List[int]]:
     transition_matrix = [[0 for _ in range(inv_limit + 1)] for _ in range(inv_limit + 1)]
     for level in range(inv_limit + 1):
         for demand, prob in demand_dist.items():
@@ -128,8 +131,9 @@ def calculate_transition_matrix(inv_limit: int, demand_dist: dict) -> list:
     return transition_matrix
 
 
-def markov_decision_process(param: Param, before_demand_dist: dict, after_demand_dist: dict) -> tuple:
-    value_res = [[0 for _ in range(param.inventory_limit + 1)] for _ in range(param.time_horizon + 1)]
+def markov_decision_process(param: Param, before_demand_dist: Dict[int, float],
+                            after_demand_dist: Dict[int, float]) -> Tuple[List[List[float]], List[List[int]]]:
+    value_res = [[0.0 for _ in range(param.inventory_limit + 1)] for _ in range(param.time_horizon + 1)]
     action_res = [[0 for _ in range(param.inventory_limit + 1)] for _ in range(param.time_horizon + 1)]
 
     for inv_level in range(param.inventory_limit + 1):
@@ -157,7 +161,8 @@ def markov_decision_process(param: Param, before_demand_dist: dict, after_demand
     return value_res, action_res
 
 
-def calculate_max_lost_demand(param: Param, before_demand_dist: dict, after_demand_dist: dict, action: list) -> float:
+def calculate_max_lost_demand(param: Param, before_demand_dist: Dict[int, float], after_demand_dist: Dict[int, float],
+                              action: List[List[int]]) -> float:
     max_lost = 0
     for period in range(param.time_horizon):
         if period < param.shift_period:
@@ -183,15 +188,25 @@ if __name__ == '__main__':
 
     max_lost_demand = calculate_max_lost_demand(param_instance, before_demand_distribution, after_demand_distribution,
                                                 optimal_action)
-    print(max_lost_demand)
+    print('maximum lost demand in one epoch:', max_lost_demand)
 
-    optimal_value_matrix = np.transpose(np.matrix(optimal_value))
-    optimal_action_matrix = np.transpose(np.matrix(optimal_action))
+    optimal_value_matrix = np.transpose(optimal_value)
+    optimal_action_matrix = np.transpose(optimal_action)
+    print('optimal total net profit in all epochs with no inventory at the beginning:', optimal_value_matrix[0][0])
 
-    with open('optimal_value.txt', 'wb') as f:
-        for line in optimal_value_matrix:
-            np.savetxt(f, line, fmt='%i')
+    data_index = []
+    for row in range(param_instance.inventory_limit + 1):
+        row_name = 'Inventory Level = ' + str(row)
+        data_index.append(row_name)
 
-    with open('optimal_action.txt', 'wb') as f:
-        for line in optimal_action_matrix:
-            np.savetxt(f, line, fmt='%i')
+    data_columns = []
+    for column in range(1, param_instance.time_horizon + 1):
+        column_name = 'Decision Epoch = ' + str(column)
+        data_columns.append(column_name)
+    data_columns.append('Dummy Period')
+
+    optimal_value_matrix = pd.DataFrame(data=optimal_value_matrix, index=data_index, columns=data_columns)
+    optimal_action_matrix = pd.DataFrame(data=optimal_action_matrix, index=data_index, columns=data_columns)
+
+    optimal_value_matrix.to_csv('optimal_value.csv')
+    optimal_action_matrix.to_csv('optimal_action.csv')
